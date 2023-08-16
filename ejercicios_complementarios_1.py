b854675ecec93ae66e54f77d8fbe97afdb23d22a
Vale-source <valentincuriel03@gmail.com>
2023-08-16 18:58:25 -0300

    primer commit

---
+++ ejercicios_complementarios_1.py
@@ -0,0 +1,59 @@
+#Ejercicio1
+numero1 = 6
+
+#Ejercicio2
+numero2 = 1.5
+
+#Ejercicio3
+suma = numero1 + numero2
+
+#Ejercicio4
+resta = numero1 - numero2
+multiplicacion = numero1*numero2
+division = numero1/numero2
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+
+#Ejercicio5
+nombre = "Valentin"
+
+#Ejercicio6
+precio = 35.60
+
+#Ejercicio7
+descuento = 0.30
+
+#Ejercicio8
+precio_final = precio - (precio*descuento)
+
+#Ejercicio9
+cadena = "Estamos en eso"
+
+#Ejercicio10
+longitud = len(cadena)
+
+#Ejercicio11
+precio = int(45.68)
+
+#Ejercicio12
+nombre = "Valentin"
+apellido = "Curiel Galeno"
+nombre_completo = nombre + apellido
+
+#Ejercicio13
+mi_edad = 20
+mi_edad += 5
+mi_edad -= 10
+
+#Ejercicio14
+altura = (1.72*4)/3
+
+#Ejercicio15
+mayusculas = "VALENTIN"
+mayusculas = mayusculas . lower()
+
+#Ejercicio16
+mayusculas = mayusculas . title()
+
